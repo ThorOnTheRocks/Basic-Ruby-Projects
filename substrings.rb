dbec4466aee4dfd402cf17cb941294c2e4e4894f
Gianluca Galota <gianluca.galota@gmail.com>
2020-11-25 15:40:42 +0000

    2nd Basic ruby project

---
+++ substrings.rb
@@ -0,0 +1,16 @@
+require 'pry'
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+def substrings (string, dictionary)
+    matches = Hash.new(0)
+    dictionary.each do |word|
+        matches[word] = string.downcase.scan(/(?=#{word})/).count if string.downcase.include?(word)
+    end
+    binding.pry
+    matches
+end
+
+puts substrings("Below", dictionary)
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
+
