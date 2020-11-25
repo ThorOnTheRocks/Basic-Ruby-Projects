95ddcb95ac37da5efef78796b29fb6f74898aaa5
Gianluca Galota <gianluca.galota@gmail.com>
2020-11-25 15:42:17 +0000

    fixed bug test

--- substrings.rb
+++ substrings.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
 
 def substrings (string, dictionary)
@@ -7,7 +5,7 @@ def substrings (string, dictionary)
     dictionary.each do |word|
         matches[word] = string.downcase.scan(/(?=#{word})/).count if string.downcase.include?(word)
     end
-    binding.pry
+
     matches
 end
 
